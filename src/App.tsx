545b329ffa4b036ce50a117a8a8a6cd6bcc6173e
Jean-Louis Leysens <jloleysens@gmail.com>
2022-03-29 10:12:06 +0200

    fix indentation and removed old sample data

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,6 @@ import { AddEntry } from './containers';
 
 import { useJournalEntries } from './state';
 
-// const entries = [
-//   {
-//     date: new Date(),
-//     description: 'Bacon and a lot of other stuff ',
-//   },
-//   {
-//     date: new Date(),
-//     description:
-//       'Bacon and a lot of other stuff Bacon and a lot of other stuff',
-//   },
-//   {
-//     date: new Date(),
-//     description:
-//       'Bacon and a lot of other stuff Bacon and a lot of other stuff',
-//   },
-//   {
-//     date: new Date(),
-//     description:
-//       'Bacon and a lot of other stuff Bacon and a lot of other stuff',
-//   },
-// ];
-
-// const days = [{ entries }, { entries }, { entries }];
-// const days = [];
-
 function App() {
   const days = useJournalEntries();
 
@@ -37,23 +12,23 @@ function App() {
     return <div>Loading...</div>;
   }
 
-    if (days.state === 'hasError') {
-      return <div>Something went wrong...</div>;
-    }
+  if (days.state === 'hasError') {
+    return <div>Something went wrong...</div>;
+  }
 
-    return (
-      <>
-        <div className="phone-content-width sm:w-96 md:w-144 mx-auto my-0 overflow-auto pt-10 pb-24 bg-lightBg text-black leading-6 h-full">
-          {days.contents.map((day, index) => (
-            <React.Fragment key={index}>
-              <Day key={index} {...day} />
-              {index !== days.contents.length - 1 && <SpacerVS />}
-            </React.Fragment>
-          ))}
-          <AddEntry />
-        </div>
-      </>
-    );
+  return (
+    <>
+      <div className="phone-content-width sm:w-96 md:w-144 mx-auto my-0 overflow-auto pt-10 pb-24 bg-lightBg text-black leading-6 h-full">
+        {days.contents.map((day, index) => (
+          <React.Fragment key={index}>
+            <Day key={index} {...day} />
+            {index !== days.contents.length - 1 && <SpacerVS />}
+          </React.Fragment>
+        ))}
+        <AddEntry />
+      </div>
+    </>
+  );
 }
 
 export default App;
